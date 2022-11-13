117f9adc9bd109dc7b3524e8aa386f603bb73f57
mysteriou <elio07@hotmail.fr>
2022-11-13 11:11:45 +0100

    affichage prix total

--- index.php
+++ index.php
@@ -6,11 +6,13 @@ $number = "5";
 
 $item_name = "test";
 
+$email_business = "tonymassa-facilitator@free.fr";
+
 ?>
 
 <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post">
   <input type="hidden" name="cmd" value="_xclick">
-    <input type="hidden" name="business" value="tonymassa-facilitator@free.fr">
+    <input type="hidden" name="business" value="<?php echo $email_business?>">
     <input type="hidden" name="item_name" value="<?php echo $item_name;?>">
     <input type="hidden" name="item_number" value="<?php echo $number;?>">
     <input type="hidden" name="amount" value="<?php echo $value;?>">
@@ -20,6 +22,12 @@ $item_name = "test";
     <input type="hidden" name="lc" value="AU">
     <input type="hidden" name="bn" value="PP-BuyNowBF">
   <input type="hidden" name="hosted_button_id" value="6RNT8A4HBBJRE">
+
+   <div>
+
+   total <?php echo $value." &nbsp;EUR"?>
+    </div>
+
   <input type="image" src="https://www.paypalobjects.com/webstatic/en_US/i/btn/png/btn_buynow_107x26.png" alt="Buy Now">
   <img alt="" src="https://paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
 </form>
